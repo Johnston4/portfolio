04fdc7d537fa129409550eb87ff1e078df323037
Sara Johnston <sarajohnston4@gmail.com>
2016-06-08 10:37:38 -0700

    severe overhaul of header/nav and general layout of sections

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-main.js
+$(document).ready(function(){
+// var div = $("div")
+// var nav_box = div.addClass("nav_box")
+// var nav_bubble = div.addClass("nav_bubble")
+// var a = $("a")
+
+// $("#header").append(nav_box)
+// $(".nav_box").append(nav_bubble)
+
+});
+
+function display_nav(){
+
+// <div class="nav_box">
+// <div class="nav_bubble, about">
+//    <a href="#about_me">
+//                About
+//    </a>
+// </div>
+// </div>
+
+}
+display_nav()
+function display_content(){
+	
+}
